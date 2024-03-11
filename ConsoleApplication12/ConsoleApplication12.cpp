70001c4a45116d95b087e693955e508ef386db9f
TinntPC <160477784+TinntPC@users.noreply.github.com>
2024-03-11 08:47:15 +0700

    LTCB_Forlong

--- ConsoleApplication12/ConsoleApplication12.cpp
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -4,7 +4,7 @@ int main() {
 	int so;
 	cout << "Nhap so: "; cin >> so;
 	for (int i = 0; i < so; i++) {
-		for (int j = 0; j < so; j++) {
+		for (int j = 0; j <= i; j++) {
 			cout << "* ";
 		}
 		cout << endl;
